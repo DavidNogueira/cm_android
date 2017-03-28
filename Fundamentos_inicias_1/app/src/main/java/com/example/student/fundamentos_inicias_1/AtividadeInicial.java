a4ffc119aa2ab788386179b7abac5b64cd350e4c
Alexis Albino Reis de Sousa <alexi-kingman@hotmail.com>
2017-03-28 12:01:02 +0100

    tutorial 1

---
+++ Fundamentos_inicias_1/app/src/main/java/com/example/student/fundamentos_inicias_1/AtividadeInicial.java
@@ -0,0 +1,13 @@
+package com.example.student.fundamentos_inicias_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtividadeInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividade_inicial);
+    }
+}
