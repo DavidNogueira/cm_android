544ffbb2216e4bc5a04e3a4882a10a953df5dbe3
David Nogueira <hsd_sk8@hotmail.com>
2017-03-28 18:51:05 +0100

    commit

--- Fundamentos_inicias_1/app/src/main/java/com/example/student/fundamentos_inicias_1/AtividadeInicial.java
+++ Fundamentos_inicias_1/app/src/main/java/com/example/student/fundamentos_inicias_1/AtividadeInicial.java
@@ -2,6 +2,8 @@ package com.example.student.fundamentos_inicias_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class AtividadeInicial extends AppCompatActivity {
 
@@ -9,5 +11,39 @@ public class AtividadeInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_inicial);
+
+        Toast.makeText(AtividadeInicial.this, "onCreate", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(AtividadeInicial.this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+       Log.d("TAG","onStop");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(AtividadeInicial.this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(AtividadeInicial.this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("TAG","onDestroy");
+    }
+
+
 }
