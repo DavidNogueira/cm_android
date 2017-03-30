5fecc13b7cc441323a54c8ecd86b46817340d1ef
David Nogueira <hsd_sk8@hotmail.com>
2017-03-30 17:33:52 +0100

    ultimo v3

---
+++ NavDrawer/app/src/main/java/com/example/student/navdrawer/Frag3.java
@@ -0,0 +1,29 @@
+package com.example.student.navdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag3 extends Fragment {
+
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    public Frag3(){
+
+    }
+
+    public static Frag3 newInstance(){
+
+        Frag3 fragment = new Frag3();
+        return fragment;
+
+    }
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View rootView = inflater.inflate(R.layout.frag3, container, false);
+        return rootView;
+    }
+}
