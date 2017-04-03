e3795354eb5ab63cd9a25c70f6b6ee8511ae0e6f
David Nogueira <hsd_sk8@hotmail.com>
2017-04-03 22:43:00 +0100

    inicio do all

---
+++ All_concepts/app/src/main/java/com/example/student/all_concepts/Utils.java
@@ -0,0 +1,18 @@
+package com.example.student.all_concepts;
+
+
+/**
+ * Created by david on 02/04/17.
+ */
+public class Utils {
+    public static final String param_status = "status";
+    public static final String param_dados = "dados";
+    public static final String param_nome = "nome";
+    public static final String param_email = "email";
+    public static final String output_erro = "ERRO";
+
+    public static final String NOME = "nome";
+    public static final String PASS = "pass";
+
+
+}
