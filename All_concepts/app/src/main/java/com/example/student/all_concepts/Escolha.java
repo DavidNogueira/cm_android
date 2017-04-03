2486f19f2bea4daaa76404d1cd92294c85afb9dd
David Nogueira <hsd_sk8@hotmail.com>
2017-04-03 23:36:29 +0100

    1.1

--- All_concepts/app/src/main/java/com/example/student/all_concepts/Escolha.java
+++ All_concepts/app/src/main/java/com/example/student/all_concepts/Escolha.java
@@ -1,13 +1,40 @@
 package com.example.student.all_concepts;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Escolha extends AppCompatActivity {
+    public TextView editNome;//iniciamos a textView
+    public TextView editPass;//iniciamos a textView
+    public String nome;
+    public int pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_escolha);
+
+
+
+
+        editNome = (TextView) findViewById(R.id.txtnome);//a textView que foi iniciada, é associada à que já existe
+        editPass = (TextView) findViewById(R.id.txtpass);//a textView que foi iniciada, é associada à que já existe
+
+        android.content.SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_pref_1),Context.MODE_PRIVATE);
+        //nome = sharedPref.getString("nome", null);//é o mesmo que a de baixo desta
+        nome = sharedPref.getString(Utils.NOME, null);
+        pass = sharedPref.getInt("pass", 0);
+
+        editNome.setText(String.valueOf(nome));
+        editPass.setText(String.valueOf(pass));
+
+
     }
+
 }
