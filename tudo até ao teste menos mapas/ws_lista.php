8decc86455a9e89d8cca8455b98230cdc942d6fa
Alexis Albino Reis de Sousa <alexi-kingman@hotmail.com>
2017-04-08 11:26:01 +0100

    tudo até ao teste menos os mapas

---
+++ tudo até ao teste menos mapas/ws_lista.php
@@ -0,0 +1,27 @@
+<?php
+
+
+    $meuarray = array();
+
+    array_push($meuarray, array(
+      'nome' => 'Joao',
+      'morada' => 'Viana',
+      'idade' => '27'
+     ));
+
+     array_push($meuarray, array(
+       'nome' => 'manuel',
+       'morada' => 'Porto',
+       'idade' => '20'
+      ));
+
+      array_push($meuarray, array(
+        'nome' => 'Pedro',
+        'morada' => 'Aveiro',
+        'idade' => '22'
+       ));
+
+       $result = [ 'status' => 'OK', 'dados' => $meuarray];
+       echo json_encode($result);
+  
+?>
