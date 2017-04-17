18ee3694990f7346766825b0ac7498870545b3eb
David Nogueira <hsd_sk8@hotmail.com>
2017-04-17 18:26:10 +0100

    mapa a dar

--- StudentTool/app/src/main/java/com/example/student/studenttool/Frag2.java
+++ StudentTool/app/src/main/java/com/example/student/studenttool/Frag2.java
@@ -1,15 +1,19 @@
 package com.example.student.studenttool;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Frag2 extends Fragment {
 
+    private Button button;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
     public Frag2(){
@@ -27,6 +31,34 @@ public class Frag2 extends Fragment {
 
         View rootView = inflater.inflate(R.layout.frag2, container, false);
 
+
+
+        Button btn = (Button) rootView.findViewById(R.id.button52);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //getActivity().finish();
+
+                Intent intent = new Intent(v.getContext(), Mapa.class);
+                startActivityForResult(intent, 1);
+
+            }
+        });
+
+
+
+
         return rootView;
     }
+
+
+
+
+
+
 }
